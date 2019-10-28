8a22e7921b915cc5b7244423dee8edafc89df913
ValDagon <valdk652@gmail.com>
2019-10-28 23:46:31 +0500

    Лабораторная работа 3

--- LaboratoryWork3/Form1.Designer.cs
+++ LaboratoryWork3/Form1.Designer.cs
@@ -28,13 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.XTextBox = new System.Windows.Forms.TextBox();
+            this.YTextBox = new System.Windows.Forms.TextBox();
+            this.ZTextBox = new System.Windows.Forms.TextBox();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Helvetica", 8.25F);
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Helvetica", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Helvetica", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Z:";
+            // 
+            // XTextBox
+            // 
+            this.XTextBox.Location = new System.Drawing.Point(53, 27);
+            this.XTextBox.Name = "XTextBox";
+            this.XTextBox.Size = new System.Drawing.Size(100, 20);
+            this.XTextBox.TabIndex = 3;
+            // 
+            // YTextBox
+            // 
+            this.YTextBox.Location = new System.Drawing.Point(53, 52);
+            this.YTextBox.Name = "YTextBox";
+            this.YTextBox.Size = new System.Drawing.Size(100, 20);
+            this.YTextBox.TabIndex = 4;
+            // 
+            // ZTextBox
+            // 
+            this.ZTextBox.Location = new System.Drawing.Point(53, 78);
+            this.ZTextBox.Name = "ZTextBox";
+            this.ZTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ZTextBox.TabIndex = 5;
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Location = new System.Drawing.Point(7, 125);
+            this.ResultTextBox.Multiline = true;
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.Size = new System.Drawing.Size(781, 261);
+            this.ResultTextBox.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Helvetica", 8.25F);
+            this.button1.Location = new System.Drawing.Point(7, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(348, 46);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "ПУСК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Helvetica", 8.25F);
+            this.button2.Location = new System.Drawing.Point(415, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(373, 46);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "ОЧИСТИТЬ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(179, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(101, 89);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "F(x)";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Helvetica", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(62, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "exp(x)";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Helvetica", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "cos(x)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Helvetica", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "sin(x)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ResultTextBox);
+            this.Controls.Add(this.ZTextBox);
+            this.Controls.Add(this.YTextBox);
+            this.Controls.Add(this.XTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox XTextBox;
+        private System.Windows.Forms.TextBox YTextBox;
+        private System.Windows.Forms.TextBox ZTextBox;
+        private System.Windows.Forms.TextBox ResultTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
--- LaboratoryWork3/Form1.cs
+++ LaboratoryWork3/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LaboratoryWork3
@@ -15,6 +8,96 @@ namespace LaboratoryWork3
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResultTextBox.Clear();
+            // Получение исходных данных из TextBox
+            double x = Convert.ToDouble(XTextBox.Text);
+            double y = Convert.ToDouble(YTextBox.Text);
+            double z = Convert.ToDouble(ZTextBox.Text);
+
+            // Ввод исходных данных в окно результатов
+            ResultTextBox.Text += "При X = " + XTextBox.Text + Environment.NewLine;
+            ResultTextBox.Text += "При Y = " + YTextBox.Text + Environment.NewLine;
+            ResultTextBox.Text += "При Z = " + ZTextBox.Text + Environment.NewLine;
+
+            // Определение номера выбранной функции
+            int RadioButtonValue = 0;
+            if (radioButton1.Checked) RadioButtonValue = 0;
+            else if (radioButton2.Checked) RadioButtonValue = 1;
+            else if (radioButton3.Checked) RadioButtonValue = 2;
+
+            // Вычисление U
+            double u = 0;
+            double Fx;
+            switch (RadioButtonValue)
+            {
+                case 0:
+                    Fx = Math.Sin(x);
+                    if (x > y)
+                    {
+                        u = (Math.Pow((Fx - y), 3)) + Math.Atan(Fx);
+                    }
+                    else if (y > x)
+                    {
+                        u = Math.Pow((y - Fx), 3) + Math.Atan(Fx);
+                    }
+                    else if (y == x)
+                    {
+                        u = Math.Pow((y - Fx), 3) + 0.5;
+                    }
+                    ResultTextBox.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+                    break;
+
+                case 1:
+                    Fx = Math.Cos(x);
+                    if (x > y)
+                    {
+                        u = (Math.Pow((Fx - y), 3)) + Math.Atan(Fx);
+                    }
+                    else if (y > x)
+                    {
+                        u = Math.Pow((y - Fx), 3) + Math.Atan(Fx);
+                    }
+                    else if (y == x)
+                    {
+                        u = Math.Pow((y - Fx), 3) + 0.5;
+                    }
+                    ResultTextBox.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+                    break;
+
+                case 2:
+                    Fx = Math.Exp(x);
+                    if (x > y)
+                    {
+                        u = (Math.Pow((Fx - y), 3)) + Math.Atan(Fx);
+                    }
+                    else if (y > x)
+                    {
+                        u = Math.Pow((y - Fx), 3) + Math.Atan(Fx);
+                    }
+                    else if (y == x)
+                    {
+                        u = Math.Pow((y - Fx), 3) + 0.5;
+                    }
+                    ResultTextBox.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+                    break;
+                default:
+                    ResultTextBox.Text += "Решение не найдено" + Environment.NewLine;
+                    break;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ResultTextBox.Clear();
         }
     }
 }
