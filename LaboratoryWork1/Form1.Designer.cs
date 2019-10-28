595845930c1c68e89df1944d70caca197bf25505
ValDagon <valdk652@gmail.com>
2019-10-28 21:03:46 +0500

    Выполнил первую лабораторную

---
+++ LaboratoryWork1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace LaboratoryWork1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InvisibleButton = new System.Windows.Forms.Button();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InvisibleButton
+            // 
+            this.InvisibleButton.Location = new System.Drawing.Point(134, 156);
+            this.InvisibleButton.Name = "InvisibleButton";
+            this.InvisibleButton.Size = new System.Drawing.Size(75, 23);
+            this.InvisibleButton.TabIndex = 0;
+            this.InvisibleButton.Text = "Скрыть";
+            this.InvisibleButton.UseVisualStyleBackColor = true;
+            this.InvisibleButton.Click += new System.EventHandler(this.InvisibleButton_Click);
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(215, 156);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 1;
+            this.ShowButton.Text = "Показать";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(296, 156);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 2;
+            this.ClearButton.Text = "Очистить";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 336);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.InvisibleButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button InvisibleButton;
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
