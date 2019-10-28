44d1156aa8eff2336d858a43e6ae47aad7121e65
ValDagon <valdk652@gmail.com>
2019-10-29 00:11:25 +0500

    Лабораторная работа 4

---
+++ LaboratoryWork4/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaboratoryWork4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double x0 = Convert.ToDouble(textBox1.Text);
+            double xk = Convert.ToDouble(textBox2.Text);
+            double dx = Convert.ToDouble(textBox3.Text);
+            double a = Convert.ToDouble(textBox4.Text);
+            double b = -6.25;
+            double x = x0;
+
+            while (x <= (xk + dx / 2))
+            {
+                double y = Math.Pow(a * x, 3) + Math.Pow(Math.Cos(Math.Pow(x, 3) - b), 2);
+
+                textBox5.Text += "x=" + Convert.ToString(x) + "; y=" + Convert.ToString(y) + Environment.NewLine;
+                x = x + dx;
+            }
+        }
+    }
+}
