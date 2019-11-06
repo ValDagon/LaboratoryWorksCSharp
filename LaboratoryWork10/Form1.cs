ede37db9d474654d3b55f71e2e614d7b7a303749
ValDagon <valdk652@gmail.com>
2019-11-06 11:48:38 +0500

    Лабораторная работа 10 и наполовину 11

---
+++ LaboratoryWork10/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        const float PI = (float)Math.PI;
+        readonly Timer tmr = new Timer() { Interval = 25 };
+        readonly GraphicsPath sinusoid;
+        int n;
+        public Form1()
+        {
+            InitializeComponent()
+                ;
+            pictureBox1.Paint += pictureBox1_Paint;
+            tmr.Tick += Tmr_Tick;
+            sinusoid = new GraphicsPath();
+            n = 0;
+            CreateSinusoid(sinusoid, 10f * PI, pictureBox1.ClientSize);
+
+            tmr.Start();
+        }
+
+        void Tmr_Tick(object sender, EventArgs e)
+        {
+            if (++n >= sinusoid.PointCount) n = 0;
+            pictureBox1.Refresh();
+        }
+
+        void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (sinusoid.PointCount == 0) return;
+            PointF pt = sinusoid.PathPoints[n];
+            e.Graphics.ScaleTransform(1, -1);
+            e.Graphics.TranslateTransform(5, -pictureBox1.Height / 2);
+            e.Graphics.DrawPath(Pens.Black, sinusoid);
+            e.Graphics.FillEllipse(Brushes.Red, RectangleF.FromLTRB(pt.X - 15, pt.Y - 15, pt.X + 15, pt.Y + 15));
+        }
+
+        private void CreateSinusoid(GraphicsPath gp, float MaxX, Size size)
+        {
+            gp.Reset();
+            PointF[] points = new PointF[1] { PointF.Empty };
+            for (float i = 0; i <= MaxX; i += 0.1f)
+            {
+                points[points.GetUpperBound(0)] = new PointF(i, (float)Math.Sin(i));
+                Array.Resize<PointF>(ref points, points.Length + 1);
+            }
+            Array.Resize<PointF>(ref points, points.Length - 1);
+            gp.AddCurve(points);
+            Matrix m = new Matrix();
+            m.Scale(size.Width / MaxX, 0.5f * size.Height);
+            gp.Transform(m);
+        }
+    }
+}
---
+++ LaboratoryWork11/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace LaboratoryWork11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.GrayButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(12, 400);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(264, 45);
+            this.OpenButton.TabIndex = 0;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 382);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(512, 400);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(276, 45);
+            this.SaveButton.TabIndex = 2;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // GrayButton
+            // 
+            this.GrayButton.Location = new System.Drawing.Point(283, 400);
+            this.GrayButton.Name = "GrayButton";
+            this.GrayButton.Size = new System.Drawing.Size(223, 45);
+            this.GrayButton.TabIndex = 3;
+            this.GrayButton.Text = "Gray";
+            this.GrayButton.UseVisualStyleBackColor = true;
+            this.GrayButton.Click += new System.EventHandler(this.GrayButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GrayButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.OpenButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button GrayButton;
+    }
+}
+
